431aab9b072dd360b3531f8ea0a336baae9b6b98
Renato Bezerra <rfbezerra@gmail.com>
2022-10-09 19:28:31 -0300

    Initial

---
+++ kafka-producer/src/test/java/net/rfbezerra/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rfbezerra.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
